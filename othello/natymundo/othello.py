ad64aafb4d12199a36d93f5e0b1b109a80578e9a
natymundo <49127475+natymundo@users.noreply.github.com>
2019-04-15 17:52:30 +0200

    Gestion de la fin du jeu

--- othello/natymundo/othello.py
+++ othello/natymundo/othello.py
@@ -27,16 +27,20 @@ class Othello:
             self.jeu[3][3], self.jeu[4][4] = BLANC, BLANC
         self.joueur = NOIR
         self.brouillon = None   # une copie du jeu sur laquelle on écrit plein d'infos utiles
+        self.eog = False
         self.update()  
 
     def __str__(self):
+        """ Ecrit le plateau de jeu sous forme d'un tableau """
         return '\n'.join(';'.join(str(i) for i in ligne) for ligne in self.jeu)
 
     def write(self, file):
+        """ Ecrit le plateau de jeu dans un fichier """
         with open(file, 'w', encoding='utf8') as f:
             f.write(str(self))
         
     def read(self, file):
+        """ Lit le plateau de jeu écrit dans un fichier """
         array = []
         with open(file, 'r', encoding='utf8') as f:
             for line in f:
@@ -46,12 +50,27 @@ class Othello:
         return jeu.reshape((SIZE, len(COLONNES)))
 
     def next(self):
+        """
+        Passe au joueur suivant et update le brouillon
+        Si aucune case du brouillon n'est jouable, 
+        c'est que le jeu est terminé!
+        """
         self.joueur = -self.joueur
+        self.update()
+        return not self.eog
 
     def adversaire(self, couleur):
         return -couleur
                   
     def update(self):
+        """
+        Créé une copie du plateau de jeu s'il cette copie n'existe pas déjà
+        Vérifie la jouabilité de chaque case 
+        et note un tuple des cases qui seront retournées
+        si cette case est jouée. 
+        Si aucune case n'est jouable, c'est la fin du jeu (eog = true)
+        """
+        self.eog = True
         if not self.brouillon:
             self.brouillon = []
             for l in range(SIZE):
@@ -61,6 +80,8 @@ class Othello:
         for l in range(SIZE):
             for c in range(len(COLONNES)):
                 self.brouillon[l][c] = self.isJouable(l, c, self.joueur)
+                if self.brouillon[l][c]:
+                    self.eog = False
     
    
     def casesAdjacentes(self, ligne, colonne, couleur):
@@ -105,7 +126,8 @@ class Othello:
 
     def isJouable(self, ligne, colonne, couleur):
         """
-            renvoie un tableau des cases qui seront retournées ou VIDE si le tableau est vide
+            renvoie un tableau des cases qui 
+            seront retournées ou VIDE si le tableau est vide
         """
         pos = VIDE
         if self.jeu[ligne][colonne] not in (NOIR, BLANC):
@@ -116,7 +138,8 @@ class Othello:
         return pos
 
     def meilleurCoup(self, couleur):
-        """ renvoie le couple (ligne, colonne) correspondant à la case qui retourne le plus de jetons """
+        """ renvoie le couple (ligne, colonne) 
+        correspondant à la case qui retourne le plus de jetons """
         maxSize = 0
         for l in range(SIZE):
             for c in range(len(COLONNES)):
@@ -147,8 +170,6 @@ class Othello:
         self.jeu[ligne][colonne] = self.joueur
         for (l, c) in casesARetourner:
             self.jeu[l][c] = -self.jeu[l][c]
-        self.next()
-        self.update()
 
     def toString(self, case):
         c = COLONNES[case[0]]
@@ -156,16 +177,26 @@ class Othello:
         return ''.join((c, l))
 
     def playAI(self):
-        """ Détermine la case que jouera l'AI """
+        """ Détermine la case que jouera l'AI puis joue.
+         Renvoie 'EOG' s'il n'y a plus de case jouable
+        """
         case = self.meilleurCoup(self.joueur)
         self.jouer(case[0], case[1], self.brouillon[case[0]][case[1]])
-        return self.toString(case)
+        if self.next():
+            return self.toString(case)
+        else:
+            return 'EOG'
 
     def playJoueur(self, ligne, colonne):
-        """ Verifie la jouablilité de la case """
+        """ Verifie la jouablilité de la case puis joue ou renvoie False 
+         Renvoie 'EOG' s'il n'y a plus de case jouable
+        """
         if not isinstance(self.brouillon[ligne][colonne], (int, np.int8)):
             self.jouer(ligne, colonne, self.brouillon[ligne][colonne])
-            return self.toString((ligne, colonne))
+            if self.next():
+                return self.toString((ligne, colonne))
+            else:
+                return 'EOG'
         else:
             return False
     
